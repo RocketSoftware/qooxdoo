67b805112fe50df448c0e15cc9f2ed6a53cf7ecf
Veronika Seliverstova <selver@rocketsoftware.com>
2019-04-17 14:34:50 +0200

    https://jira.rocketsoftware.com/browse/LWEB-402 - [#LWEB-402] Web client: Datepicker with fwd/bwd option does not allow to type -1 directly This change was missing, reapplied it

--- framework/source/class/qx/ui/form/Spinner.js
+++ framework/source/class/qx/ui/form/Spinner.js
@@ -683,14 +683,14 @@ qx.Class.define("qx.ui.form.Spinner",
     _onTextChange : function(e)
     {
       var textField = this.getChildControl("textfield");
-      var value;
+      var value, textFieldValue = textField.getValue();
 
       // if a number format is set
       if (this.getNumberFormat())
       {
         // try to parse the current number using the number format
         try {
-          value = this.getNumberFormat().parse(textField.getValue());
+          value = this.getNumberFormat().parse(textFieldValue);
         } catch(ex) {
           // otherwise, process further
         }
@@ -699,7 +699,7 @@ qx.Class.define("qx.ui.form.Spinner",
       if (value === undefined)
       {
         // try to parse the number as a float
-        value = parseFloat(textField.getValue());
+        value = parseFloat(textFieldValue);
       }
 
       // if the result is a number
@@ -714,7 +714,9 @@ qx.Class.define("qx.ui.form.Spinner",
 
         // If value is the same than before, call direcly _applyValue()
         if (value === this.__lastValidValue) {
+          if(textFieldValue !== "-0" || !e) {
           this._applyValue(this.__lastValidValue);
+          }
         } else {
           this.setValue(value);
         }
@@ -722,7 +724,9 @@ qx.Class.define("qx.ui.form.Spinner",
       else
       {
         // otherwise, reset the last valid value
+        if(textFieldValue !== "-" || !e) {
         this._applyValue(this.__lastValidValue, undefined);
+      }
       }
     },
 
